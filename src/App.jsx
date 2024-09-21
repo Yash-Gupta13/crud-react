9812605bbfdd121ae39964cb7995cd1732ab8d99
shivam70066 <137193105+shivam70066@users.noreply.github.com>
2024-09-22 02:32:59 +0530

    added pagination

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,39 @@ import axios from "axios";
 
 function App() {
   const [contactList, setContactList] = useState([]);
-
-  const fetchData = async()=>{
+  const [totalRecords, setTotalRecords] = useState(0);
+  const [page, setPage] = useState(1);
+  const [rows, setRows] = useState(10);
+  
+  const fetchData = async () => {
     try {
       const response = await axios.get(
-        `https://demobackend.web2.99cloudhosting.com/user/list_contacts`
+        `https://demobackend.web2.99cloudhosting.com/user/list_contacts?rows=${rows}&page=${page}&offset=${offset}`
       );
-
+      setTotalRecords(response.data.metadata.total);
+      console.log(response.data);
       const contacts = response.data.record;
       setContactList(contacts);
-      // setContactList(contactWithPics);
     } catch (error) {
-      
+      console.error("Error fetching data:", error);
     }
-  }
+  };
 
-  
-
-  useEffect(()=>{
+  useEffect(() => {
     fetchData();
-  },[])
+  }, [page]); 
+  const handleNextPage = () => {
+    if (page * rows < totalRecords) { 
+      setPage(prevPage => prevPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (page > 1) {
+      setPage(prevPage => prevPage - 1);
+    }
+  };
+
   return (
     <>
       <div className="mainContainer">
@@ -39,9 +52,18 @@ function App() {
           </div>
 
           <div className="div">
-            <Table contactList={contactList}/>
+            <Table contactList={contactList} />
           </div>
         </div>
+      </div>
+      <div>Total records: {totalRecords}</div>
+      <div>
+        <button onClick={handlePreviousPage} disabled={page === 1}>
+          Previous Page
+        </button>
+        <button onClick={handleNextPage} disabled={page * rows >= totalRecords}>
+          Next Page
+        </button>
       </div>
     </>
   );
